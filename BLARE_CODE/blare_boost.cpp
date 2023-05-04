0acb7f0978947d73f604bd770b27d2ee25cac001
LING ZHANG <junelena1313@gmail.com>
2023-05-04 17:03:05 -0500

    Update split regex in original expr code

--- BLARE_CODE/blare_boost.cpp
+++ BLARE_CODE/blare_boost.cpp
@@ -24,17 +24,30 @@
 // code a bit less verbose.
 typedef boost::mt19937 base_generator;
 
+bool char_escaped(const std::string &line, std::size_t pos) {
+    auto temp_pos = pos;
+    int num_escaped = 0;
+    // count number of escape characters before the char
+    while (temp_pos > 0) {
+        if (line.at(--temp_pos) != '\\') 
+            break;
+        num_escaped++;
+    }
+    return num_escaped % 2 == 1;
+}
+
+
 std::tuple<std::string, std::string, std::string> split_regex(const std::string &line) {
     std::size_t pos = 0;
     std::string prefix = line;
     std::string regex = "";
     std::string suffix = "";
     while ((pos = line.find("(", pos)) != std::string::npos) {
-        if (pos == 0 || line.at(pos - 1) != '\\') {
+        if (!char_escaped(line, pos)) {
             std::size_t reg_start_pos = pos;
             std::size_t reg_end_pos = pos+1;
             while ((pos = line.find(")", pos+1)) != std::string::npos) {
-                if (line.at(pos - 1) != '\\') {
+                if (!char_escaped(line, pos)) {
                     reg_end_pos = pos;
                 }
                 pos++;
@@ -52,23 +65,22 @@ std::tuple<std::string, std::string, std::string> split_regex(const std::string 
     return std::make_tuple(prefix, regex, suffix);
 }
 
-std::tuple<std::vector<std::string>, std::vector<std::string>, bool> split_regex_multi(const std::string &line) {
+std::vector<std::string> split_regex_multi(const std::string &line) {
     std::size_t pos = 0;
     std::size_t prev_pos = 0;
-    int pos2 = -1;
+    int pos2 = 0;
     std::vector<std::string> const_strings;
     std::vector<std::string> regexes;
     std::string prefix = line;
     std::string suffix = "";
     bool prefix_first = true;
     while ((pos = line.find("(", pos)) != std::string::npos) {
-        if (pos == 0 || line.at(pos - 1) != '\\') {
+        if (!char_escaped(line, pos)) {
             prefix = line.substr(prev_pos, pos-prev_pos);
             if (prev_pos == 0 && pos == 0) prefix_first = false;
-
             pos2 = pos;
             while ((pos2 = line.find(")", pos2)) != std::string::npos) {
-                if (line.at(pos2 - 1) != '\\') {
+                if (!char_escaped(line, pos2)) {
                     suffix = line.substr(pos, pos2 + 1 -pos);
                     break;
                 }
@@ -89,16 +101,17 @@ std::tuple<std::vector<std::string>, std::vector<std::string>, bool> split_regex
             pos = pos2;
         } else {
             pos++;
-
         }
     }
+
     prefix = line.substr(pos2);
     if (!prefix.empty()) {
         prefix.erase(std::remove(prefix.begin(), prefix.end(), '\\'), prefix.end()); // remove escape (trick method. should only remove single \ and turning \\ to \)
         const_strings.push_back(prefix);
     }
-    return std::tuple<std::vector<std::string>, std::vector<std::string>, bool>(const_strings, regexes, prefix_first);
+    return const_strings;
 }
+
 
 bool MultiMatchSingle (const std::string & line, std::vector<std::shared_ptr<boost::regex>> & c_regs, std::shared_ptr<boost::regex> & reg0, const std::vector<std::string> prefixes, const std::vector<std::string> & regs, bool prefix_first, std::vector<size_t> & prev_prefix_pos) {
     boost::smatch what;
--- BLARE_CODE/blare_pcre2.cpp
+++ BLARE_CODE/blare_pcre2.cpp
@@ -28,23 +28,35 @@ typedef boost::mt19937 base_generator;
 
 typedef jpcre2::select<char> jp;
 
+bool char_escaped(const std::string &line, std::size_t pos) {
+    auto temp_pos = pos;
+    int num_escaped = 0;
+    // count number of escape characters before the char
+    while (temp_pos > 0) {
+        if (line.at(--temp_pos) != '\\') 
+            break;
+        num_escaped++;
+    }
+    return num_escaped % 2 == 1;
+}
+
+
 std::tuple<std::string, std::string, std::string> split_regex(const std::string &line) {
     std::size_t pos = 0;
     std::string prefix = line;
     std::string regex = "";
     std::string suffix = "";
     while ((pos = line.find("(", pos)) != std::string::npos) {
-        if (pos == 0 || line.at(pos - 1) != '\\') {
+        if (!char_escaped(line, pos)) {
             std::size_t reg_start_pos = pos;
             std::size_t reg_end_pos = pos+1;
             while ((pos = line.find(")", pos+1)) != std::string::npos) {
-                if (line.at(pos - 1) != '\\') {
+                if (!char_escaped(line, pos)) {
                     reg_end_pos = pos;
                 }
                 pos++;
             }
             prefix = line.substr(0, reg_start_pos);
-
             regex = line.substr(reg_start_pos, reg_end_pos+1-reg_start_pos);
             suffix = line.substr(reg_end_pos+1);
             
@@ -57,23 +69,22 @@ std::tuple<std::string, std::string, std::string> split_regex(const std::string 
     return std::make_tuple(prefix, regex, suffix);
 }
 
-std::tuple<std::vector<std::string>, std::vector<std::string>, bool> split_regex_multi(const std::string &line) {
+std::vector<std::string> split_regex_multi(const std::string &line) {
     std::size_t pos = 0;
     std::size_t prev_pos = 0;
-    int pos2 = -1;
+    int pos2 = 0;
     std::vector<std::string> const_strings;
     std::vector<std::string> regexes;
     std::string prefix = line;
     std::string suffix = "";
     bool prefix_first = true;
     while ((pos = line.find("(", pos)) != std::string::npos) {
-        if (pos == 0 || line.at(pos - 1) != '\\') {
+        if (!char_escaped(line, pos)) {
             prefix = line.substr(prev_pos, pos-prev_pos);
             if (prev_pos == 0 && pos == 0) prefix_first = false;
-
             pos2 = pos;
             while ((pos2 = line.find(")", pos2)) != std::string::npos) {
-                if (line.at(pos2 - 1) != '\\') {
+                if (!char_escaped(line, pos2)) {
                     suffix = line.substr(pos, pos2 + 1 -pos);
                     break;
                 }
@@ -94,16 +105,17 @@ std::tuple<std::vector<std::string>, std::vector<std::string>, bool> split_regex
             pos = pos2;
         } else {
             pos++;
-
         }
     }
+
     prefix = line.substr(pos2);
     if (!prefix.empty()) {
         prefix.erase(std::remove(prefix.begin(), prefix.end(), '\\'), prefix.end()); // remove escape (trick method. should only remove single \ and turning \\ to \)
         const_strings.push_back(prefix);
     }
-    return std::tuple<std::vector<std::string>, std::vector<std::string>, bool>(const_strings, regexes, prefix_first);
+    return const_strings;
 }
+
 
 bool FullMatchSingle (const std::string & line, jp::VecNum* vec_num, jp::RegexMatch & rm) {
     return rm.setSubject(line).setNumberedSubstringVector(vec_num).match();
