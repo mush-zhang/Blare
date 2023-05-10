350cba32c34270ec6cfec02848ac8c2b4db54287
LING ZHANG <junelena1313@gmail.com>
2023-05-10 14:59:09 -0500

    Fix typo and cmake

--- include/misc/regex_split.hpp
+++ include/misc/regex_split.hpp
@@ -9,7 +9,7 @@
 std::tuple<std::string, std::string, std::string> split_regex(const std::string &line);
 
 // multi-way-split
-std::vector<std::string> split_regex_multi(const std::string &line);
+std::tuple<std::vector<std::string>, std::vector<std::string>, bool> split_regex_multi(const std::string &line);
 
 
 #endif // MISC_REGEX_SPLIT_HPP_
