07e093168f3ae9e16afe9cb0bf1d4abb8cf845cd
LING ZHANG <junelena1313@gmail.com>
2023-05-22 14:15:24 -0400

    add missing typedef for pcre2

--- include/blare_pcre2/direct_match.hpp
+++ include/blare_pcre2/direct_match.hpp
@@ -8,6 +8,8 @@
 #ifndef BLARE_PCRE2_DIRECT_MATCH_HPP_
 #define BLARE_PCRE2_DIRECT_MATCH_HPP_
 
+typedef jpcre2::select<char> jp;
+
 std::pair<double, int> DirectMatchPCRE2 (const std::vector<std::string> & lines, const std::string & reg_string) {
     auto start = std::chrono::high_resolution_clock::now();
     int count = 0;
