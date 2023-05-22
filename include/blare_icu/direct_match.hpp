b3c7c6fca5d830858420fe6753134395e4dd1e5c
LING ZHANG <junelena1313@gmail.com>
2023-05-22 16:45:47 -0400

    Update direct_match.hpp

--- include/blare_icu/direct_match.hpp
+++ include/blare_icu/direct_match.hpp
@@ -12,10 +12,10 @@
 std::pair<double, int> DirectMatchICU (const std::vector<icu_72::UnicodeString> & lines, icu_72::UnicodeString reg_string) {
     auto start = std::chrono::high_resolution_clock::now();
     int count = 0;
-    UErrorCode status = U_ZERO_ERROR;
+    icu_72::UErrorCode status = icu_72::U_ZERO_ERROR;
 
-    RegexPattern* reg = RegexPattern::compile(reg_string, 0, status);
-    RegexMatcher* matcher = reg->matcher(status);
+    icu_72::RegexPattern* reg = icu_72::RegexPattern::compile(reg_string, 0, status);
+    icu_72::RegexMatcher* matcher = reg->matcher(status);
 
     for (const auto & line : lines) {
         matcher->reset(line);
@@ -27,6 +27,5 @@ std::pair<double, int> DirectMatchICU (const std::vector<icu_72::UnicodeString> 
     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
     return std::make_pair(elapsed_seconds.count(), count);
 }
-
 
 #endif // BLARE_RE2_DIRECT_MATCH_HPP_
