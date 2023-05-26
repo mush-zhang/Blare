e5e837fbf9ecedb3f57ba917349b2fb214ae2c63
LING ZHANG <junelena1313@gmail.com>
2023-05-26 11:53:11 -0400

    UPDATE CMAKE

--- cmake/FindRe2.cmake
+++ cmake/FindRe2.cmake
@@ -1,12 +1,9 @@
 include(FindPackageHandleStandardArgs)
 
 if(NOT re2_NO_re2_CMAKE)
-  # do a find package call to specifically look for the CMake version
-  # of Cbc
+
   find_package(re2 QUIET NO_MODULE)
 
-  # if we found the Cbc cmake package then we are done, and
-  # can print what we found and return.
   if(re2_FOUND)
     find_package_handle_standard_args(re2 CONFIG_MODE)
     return()
